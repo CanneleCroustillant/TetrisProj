2a9fed46682ec258ff567fb7854bdff1ab6f537e
Unknown <alexis.reignoux@gmail.com>
2017-05-23 16:33:07 +0200

    re
    qsg

---
+++ src/main/J2EE/Object/Tetrimino.java
@@ -0,0 +1,15 @@
+package Object;
+
+public class Tetrimino
+{
+	
+	private int id;
+	private String forme;
+	private String color;
+		
+	public Tetrimino()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
