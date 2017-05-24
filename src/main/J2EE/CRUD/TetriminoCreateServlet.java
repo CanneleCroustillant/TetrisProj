9fb37cdf6b4daee6d29c9f782ba48f2bcdb350e8
Unknown <alexis.reignoux@gmail.com>
2017-05-24 10:39:16 +0200

    Create tetrimino done
    not tested yet

--- src/main/J2EE/CRUD/TetriminoCreateServlet.java
+++ src/main/J2EE/CRUD/TetriminoCreateServlet.java
@@ -1,6 +1,8 @@
 package CRUD;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,13 +24,15 @@ public class TetriminoCreateServlet extends HttpServlet
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
+	{		
 		String nom = request.getParameter("nom");
 		String couleur = request.getParameter("couleur");
 		
 		Tetrimino t = new Tetrimino(nom, couleur);
 		
-		
+		@SuppressWarnings("unchecked")
+		HashMap<String, Tetrimino> listeTetrimino = (HashMap<String, Tetrimino>) request.getServletContext().getAttribute("listeTetrimino");
+		listeTetrimino.put(t.getId(), t);
 	}
 
 	/**
