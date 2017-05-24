0fe22d7f9beacb65cc7d570844b8d03f8479418c
Unknown <alexis.reignoux@gmail.com>
2017-05-24 09:08:49 +0200

    Auto stash before merge of "tetriminoSerlet" and "develop"

--- src/main/J2EE/Object/Tetrimino.java
+++ src/main/J2EE/Object/Tetrimino.java
@@ -1,15 +1,53 @@
 package Object;
 
+import java.util.UUID;
+
 public class Tetrimino
 {
 	
-	private int id;
-	private String forme;
+	private String id;
+	private String name;
 	private String color;
 		
-	public Tetrimino()
+	public Tetrimino(String nom, String couleur)
 	{
-		// TODO Auto-generated constructor stub
+		this.id = UUID.randomUUID().toString();
+		this.name = nom;
+		this.color = couleur;
 	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param nom the name to set
+	 */
+	public void setName(String nom)
+	{
+		this.name = nom;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor()
+	{
+		return color;
+	}
+
+	/**
+	 * @param couleur the color to set
+	 */
+	public void setColor(String couleur)
+	{
+		this.color = couleur;
+	}
+	
+	
 
 }
