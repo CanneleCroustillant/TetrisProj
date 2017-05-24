3ea747ad8a323a5f93eb7bbafa6447d4505b232b
Unknown <alexis.reignoux@gmail.com>
2017-05-24 09:06:22 +0200

    tetriminoServlet
    création du servlet d'accès à tetrimino.jsp

--- src/main/J2EE/Servlet/TetriminoServlet.java
+++ src/main/J2EE/Servlet/TetriminoServlet.java
@@ -10,24 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TetriminoServlet
  */
-@WebServlet("/TetriminoServlet")
+@WebServlet("/tetrimino")
 public class TetriminoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TetriminoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/tetrimino.jsp").forward(request, response);
 	}
 
 	/**
